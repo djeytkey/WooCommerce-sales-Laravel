d264e5d08dbdfa078ef8dcbde33320b1426393e4
djeytkey <tarik.engineering@gmail.com>
2025-06-28 14:08:28 +0100

    fix middleware in web routes file

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use BoukjijTarik\WooSales\Http\Controllers\WooOrdersController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'prefix' => 'woo-orders',
-    'middleware' => ['web']
-], function () {
+Route::group(['prefix' => 'woo-orders', 'middleware' => 'auth:admin'], function () {
     Route::get('/', [WooOrdersController::class, 'index'])->name('woo-orders.index');
     Route::get('/data', [WooOrdersController::class, 'getData'])->name('woo-orders.data');
     Route::post('/export', [WooOrdersController::class, 'export'])->name('woo-orders.export');
